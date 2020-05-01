4608aa2dea2af8ebbf9e06619948d897ff9153d2
Abhee Hudani <hudaniabhee@gmail.com>
2020-05-02 01:48:07 +0530

    Init with DetailedPage

---
+++ NotesKeeper/App.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using NotesKeeper.Services;
+using NotesKeeper.Views;
+
+namespace NotesKeeper
+{
+    public partial class App : Application
+    {
+
+        public App()
+        {
+            InitializeComponent();
+
+            DependencyService.Register<MockDataStore>();
+            MainPage = new MainPage();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
